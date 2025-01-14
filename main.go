773a2eef6a3a4e9bcdb3a1abcb55bedf9e3ee212
keklik-wq <jds1032@mail.ru>
2025-01-14 15:49:44 +0300

    add parallelism

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func BinarySearch(arr []int, target int) int {
@@ -23,13 +24,32 @@ func BinarySearch(arr []int, target int) int {
 }
 
 func main() {
-	arr := []int{1, 3, 5, 7, 9, 11, 13, 15, 17, 19}
-	target := 7
+	arr := []int{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 25, 90, 110, 250, 252, 254, 256, 258}
+	targets := []int{7, 11, 20, 1, 258}
 
-	result := BinarySearch(arr, target)
-	if result != -1 {
-		fmt.Printf("Элемент %d найден на индексе %d.\n", target, result)
-	} else {
-		fmt.Printf("Элемент %d не найден.\n", target)
+	results := make(chan string)
+
+	var wg sync.WaitGroup
+
+	for _, target := range targets {
+		wg.Add(1)
+		go func(target int) {
+			defer wg.Done()
+			result := BinarySearch(arr, target)
+			if result != -1 {
+				results <- fmt.Sprintf("Элемент %d найден на индексе %d.", target, result)
+			} else {
+				results <- fmt.Sprintf("Элемент %d не найден.", target)
+			}
+		}(target)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for res := range results {
+		fmt.Println(res)
 	}
 }
